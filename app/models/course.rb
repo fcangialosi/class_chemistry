4e11e8bb75f303ab02483dfa250822cf96be7051
Frank Cangialosi <fcangialosi@studyblue.com>
2013-07-05 02:06:28 -0500

    added annotations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id          :integer          not null, primary key
+#  department  :string(255)
+#  course_num  :integer
+#  course_code :string(255)
+#  name        :string(255)
+#  credits     :integer
+#  professor   :string(255)
+#  description :text
+#  perm_req    :boolean          default(FALSE)
+#  prereq      :text
+#  restriction :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Course < ActiveRecord::Base
   # attr_accessible :title, :body
 end
--- test/unit/section_test.rb
+++ test/unit/section_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: sections
+#
+#  id                  :integer          not null, primary key
+#  course_id           :integer
+#  section_num         :integer
+#  professor           :string(255)
+#  seats_total         :integer
+#  seats_open          :integer
+#  seats_waitlist      :integer
+#  lecture_building    :string(255)
+#  lecture_room        :integer
+#  lecture_times       :string(255)
+#  discussion_building :string(255)
+#  discussion_room     :integer
+#  discussion_times    :string(255)
+#  freshman_connection :boolean          default(FALSE)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'test_helper'
 
 class SectionTest < ActiveSupport::TestCase
