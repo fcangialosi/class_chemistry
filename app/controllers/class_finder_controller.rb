1c1f5e0df38be6f06ea94450698d97f050b9d4b5
Frank Cangialosi <fcangialosi@studyblue.com>
2013-07-08 02:16:19 -0500

    temporary method of randomizing colors, need to do heavy css class name modification and get rid of isotope defaults

--- app/controllers/class_finder_controller.rb
+++ app/controllers/class_finder_controller.rb
@@ -9,7 +9,19 @@ class ClassFinderController < ApplicationController
   		end
   	  @courses.push(course)
   	end
-   
+
+    @types = []
+    30.times do |x|
+      num = Random.rand(1..(6+1))
+      case num
+        when 1 then @types.push("element alkali metal")
+        when 2 then @types.push("element alkaline-earth metal")
+        when 3 then @types.push("element halogen nonmetal")
+        when 4 then @types.push("element metalloid")
+        when 5 then @types.push("element transition metal")
+        else @types.push("element noble-gas nonmetal")
+      end
+    end
   end
 
 end
