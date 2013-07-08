24a6613d57e2d17b5dc5f2c2a9ff15991942aa86
Frank Cangialosi <fcangialosi@studyblue.com>
2013-07-08 01:33:00 -0500

    working on basic styling

--- app/controllers/class_finder_controller.rb
+++ app/controllers/class_finder_controller.rb
@@ -2,8 +2,12 @@ class ClassFinderController < ApplicationController
   
   def index
   	@courses = []
-  	10.times do |x|
-  	  @courses.push(Course.offset(rand(Course.count)).first)
+  	30.times do |x|
+  		course = Course.offset(rand(Course.count)).first
+  		while(!course.undergrad)
+			course = Course.offset(rand(Course.count)).first
+  		end
+  	  @courses.push(course)
   	end
    
   end
